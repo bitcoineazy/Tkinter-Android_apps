a9a34de9ccb8aa01e15de8a4c41ea5e047672def
Matvey <bitcoineazy@gmail.com>
2021-05-21 21:03:59 +0300

    class Figure for working with all objects

--- Manipulations_flat_objects/tkinter_objects.py
+++ Manipulations_flat_objects/tkinter_objects.py
@@ -1,20 +1,44 @@
 from tkinter import *
 
-class Rectangle:
-    def __init__(self, canvas, x1, y1, x2, y2):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
+
+
+class Figure:
+    def __init__(self, canvas, **coords):
+        self.__dict__.update(coords)
         self.canvas = canvas
-        self.gen_rect = []
+        self.rectangles = []
+        self.triangles = []
+        self.hexagons = []
+        self.rectangles_generated = False
+        self.triangles_generated = False
+        self.hexagons_generated = False
 
     def create_rectangles(self):
-        for i in range(10):
-            self.gen_rect.append(self.canvas.create_rectangle(
-                self.x1 + (i*40), self.y1, self.x2 + (i*40), self.y2, fill='black'))  # right
-            self.gen_rect.append(self.canvas.create_rectangle(
-                self.x1 - (i*40), self.y1, self.x2 - (i*40), self.y2, fill='black'))  # left
+        while not self.rectangles_generated:
+            for i in range(1000):
+                self.rectangles.append(self.canvas.create_rectangle(
+                    self.x1 + (i*40), self.y1, self.x2 + (i*40), self.y2, fill='black'))
+                self.rectangles.append(self.canvas.create_rectangle(
+                    self.x1 - (i*40), self.y1, self.x2 - (i*40), self.y2, fill='black'))
+            self.rectangles_generated = True
+        for each in self.rectangles:
+            self.canvas.move(each, -1, 0)
+        self.canvas.after(10, self.create_rectangles)
+
+
+    def create_triangles(self):
+        while not self.triangles_generated:
+            for i in range(1000):
+                self.triangles.append(self.canvas.create_polygon(
+                    self.x1 + (i*40), self.y1, self.x2  + (i*40), self.y2, self.x3  + (i*40), self.y3
+                ))
+                self.triangles.append(self.canvas.create_polygon(
+                    self.x1 - (i*40), self.y1, self.x2  - (i*40), self.y2, self.x3  - (i*40), self.y3
+                ))
+            self.triangles_generated = True
+        for each in self.triangles:
+            self.canvas.move(each, 1, 0)
+        self.canvas.after(10, self.create_triangles)
 
 
 class Objects(Frame):
@@ -28,20 +52,30 @@ class Objects(Frame):
 
 
     def initUI(self):
+        self.canvas_area = Button(
+            self, text='Поле', width=10, command=self.make_canvas)
         self.rectangles = Button(
             self, text='gen_rectangle()',
              command=self.gen_rectangle, width=16)
-        self.rectangles.grid(row=0, column=0)
+        triangles = Button(self, text='gen_triangles()', command=self.gen_triangle, width=16)
+        self.rectangles.grid(row=0, column=1)
+        self.canvas_area.grid(row=0, column=0)
+        triangles.grid(row=0, column=2)
 
-
+    def make_canvas(self):
+        self.canvas_window = Toplevel(self)
+        self.canvas = Canvas(self.canvas_window, width=500, height=500)
+        self.canvas.grid(row=0, column=0)
+        self.canvas.focus()
 
     def gen_rectangle(self):
-        self.gen_rectangle = Toplevel(self)
-        self.canvas = Canvas(self.gen_rectangle, width=500, height=500)
-        self.canvas.grid(row=0, column=0)
-        self.all_rects = Rectangle(self.canvas, 250, 250, 275, 275)
+        self.all_rects = Figure(self.canvas, x1=250, y1=250, x2=275, y2=275)
         self.all_rects.create_rectangles()
-        self.canvas.focus()
+
+    def gen_triangle(self):
+        self.all_triangles = Figure(self.canvas, x1=300, y1=250, x2=220, y2=330, x3=265, y3=250)
+        self.all_triangles.create_triangles()
+
 
     def centerWindow(self):
         w = 400
