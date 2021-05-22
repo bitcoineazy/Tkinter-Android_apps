c9a6050601a3e34d5152548919b500079d08d443
Matvey <bitcoineazy@gmail.com>
2021-05-22 20:52:52 +0300

    gen_hexagons()

--- Manipulations_flat_objects/tkinter_objects.py
+++ Manipulations_flat_objects/tkinter_objects.py
@@ -1,7 +1,6 @@
 from tkinter import *
 
 
-
 class Figure:
     def __init__(self, canvas, **coords):
         self.__dict__.update(coords)
@@ -21,9 +20,9 @@ class Figure:
                 self.rectangles.append(self.canvas.create_rectangle(
                     self.x1 - (i*40), self.y1, self.x2 - (i*40), self.y2, fill='black'))
             self.rectangles_generated = True
-        for each in self.rectangles:
-            self.canvas.move(each, -1, 0)
-        self.canvas.after(10, self.create_rectangles)
+        #for each in self.rectangles:
+        #    self.canvas.move(each, -1, 0)
+        #self.canvas.after(10, self.create_rectangles)
 
 
     def create_triangles(self):
@@ -39,6 +38,25 @@ class Figure:
         for each in self.triangles:
             self.canvas.move(each, 1, 0)
         self.canvas.after(10, self.create_triangles)
+
+
+    def create_hexagons(self):
+        while not self.hexagons_generated:
+            for i in range(1000):
+                self.hexagons.append(self.canvas.create_polygon(
+                    self.x4 + (i*80), self.y4, self.x5  + (i*80), self.y5, self.x6  + (i*80), self.y6,
+                    self.x1 + (i*80), self.y1, self.x2  + (i*80), self.y2, self.x3  + (i*80), self.y3,
+                ))
+                self.hexagons.append(self.canvas.create_polygon(
+                    self.x1 - (i*80), self.y1, self.x2  - (i*80), self.y2, self.x3  - (i*80), self.y3,
+                    self.x4 - (i*80), self.y4, self.x5  - (i*80), self.y5, self.x6  - (i*80), self.y6,
+                ))
+            self.hexagons_generated = True
+        for each in self.hexagons:
+            self.canvas.move(each, 10, -3)
+
+                #self.canvas.move(each, -10, 3)
+        self.canvas.after(10, self.create_hexagons)
 
 
 class Objects(Frame):
@@ -58,14 +76,19 @@ class Objects(Frame):
             self, text='gen_rectangle()',
              command=self.gen_rectangle, width=16)
         triangles = Button(self, text='gen_triangles()', command=self.gen_triangle, width=16)
+        hexagons = Button(self, text='gen_hexagons()', command=self.gen_hexagon, width=16)
         self.rectangles.grid(row=0, column=1)
         self.canvas_area.grid(row=0, column=0)
         triangles.grid(row=0, column=2)
+        hexagons.grid(row=0, column=3)
 
     def make_canvas(self):
         self.canvas_window = Toplevel(self)
         self.canvas = Canvas(self.canvas_window, width=500, height=500)
         self.canvas.grid(row=0, column=0)
+        # TODO: frontend
+        self.canvas.create_line(0, 250, 500, 250, width=2)
+        self.canvas.create_line(250, 500, 250, 0, width=2)
         self.canvas.focus()
 
     def gen_rectangle(self):
@@ -73,20 +96,23 @@ class Objects(Frame):
         self.all_rects.create_rectangles()
 
     def gen_triangle(self):
-        self.all_triangles = Figure(self.canvas, x1=300, y1=250, x2=220, y2=330, x3=265, y3=250)
+        self.all_triangles = Figure(self.canvas, x1=300, y1=250, x2=285, y2=330, x3=265, y3=250)
         self.all_triangles.create_triangles()
+
+    def gen_hexagon(self):
+        self.all_hexagons = Figure(self.canvas, x1=235, y1=224, x2=265, y2=224, x3=280, y3=250,
+                                   x4=265, y4=276, x5=235, y5=276, x6=220, y6=250)
+        self.all_hexagons.create_hexagons()
 
 
     def centerWindow(self):
-        w = 400
+        w = 660
         h = 32
         sw = self.parent.winfo_screenwidth()
         sh = self.parent.winfo_screenheight()
         x = (sw - w) / 2
         y = (sh - h) / 2
         self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
-
-
 
 
 def main():
